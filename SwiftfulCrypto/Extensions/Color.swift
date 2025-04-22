562788f51169e8840a2fd3569476669e88a0bd28
DanutPopa <popa.ion.danut@gmail.com>
2025-04-22 21:09:33 +0300

    Setting up Xcode Project and a custom color theme.

---
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by Danut Popa on 22.04.2025.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
